c6ca472e5b728f9a212b6120720b7c6d1bba0e92
Vivek Patel <vivekp@bpl.netlink.com>
2019-11-20 19:58:30 +0530

    Outsystems

---
+++ demo/README.md
@@ -0,0 +1,20 @@
+# Cordova Demo-App
+
+## Usage
+
+Update your SDK credentials in www/js/index.js and run the following commands.
+
+### iOS
+
+```
+cd platforms/ios
+pod install
+cd ../..
+cordova run ios
+```
+
+### Android
+
+```
+cordova run android
+```
---
+++ src/ios/JumioMobileSDK.h
@@ -0,0 +1,25 @@
+//
+//  JumioMobileSDK.h
+//  Jumio Software Development GmbH
+//
+
+#import "Cordova/CDVPlugin.h"
+@import JumioCore;
+@import Netverify;
+@import NetverifyFace;
+@import BAMCheckout;
+
+@interface JumioMobileSDK : CDVPlugin <NetverifyViewControllerDelegate, AuthenticationControllerDelegate, BAMCheckoutViewControllerDelegate, DocumentVerificationViewControllerDelegate>
+
+@property (strong) NetverifyViewController* netverifyViewController;
+@property (strong) NetverifyConfiguration* netverifyConfiguration;
+@property (strong) AuthenticationController* authenticationController;
+@property (strong) AuthenticationConfiguration* authenticationConfiguration;
+@property (strong) UIViewController *authenticationScanViewController;
+@property (strong) BAMCheckoutViewController* bamViewController;
+@property (strong) BAMCheckoutConfiguration* bamConfiguration;
+@property (strong) DocumentVerificationConfiguration* documentVerifcationConfiguration;
+@property (strong) DocumentVerificationViewController* documentVerificationViewController;
+@property (strong) NSString* callbackId;
+
+@end
